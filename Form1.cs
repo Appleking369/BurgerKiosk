346eed03c51f140d4b36a41c0dcb7a2c402c6206
지혁 박 <jhboy0714@gmail.com>
2026-04-09 17:26:45 +0900

    과제2 코딩과 테스트 완료 아무것도 선택하지 않고 주문하기 버튼을 눌렀을 경우, lblTotalCost에 총 금액 대신 "메뉴를 선택하세요."가 빨간색으로 출력되도록 함 해당 상태에서 정상적으로 메뉴를 고르거나 초기화 버튼을 눌렀을 경우, 다시 원래의 색으로 텍스트를 출력함

--- Form1.cs
+++ Form1.cs
@@ -46,7 +46,14 @@ namespace BurgerKiosk
                 totalCost += 500;
                 lstOrder.Items.Add("소스추가 500원");
             }
+            lblTotalCost.ForeColor = Color.Blue;
             lblTotalCost.Text = "총 금액: " + totalCost.ToString("N0") + "원";
+
+            if (!rdoHamBurger.Checked && !rdoBulgogiBurger.Checked && !rdoChickenBurger.Checked)
+            {
+                lblTotalCost.Text = "메뉴를 선택하세요.";
+                lblTotalCost.ForeColor = Color.Red;
+            }
         }
 
         private void btnClear_Click(object sender, EventArgs e)
@@ -62,6 +69,7 @@ namespace BurgerKiosk
 
             lstOrder.Items.Clear(); 
             totalCost = 0;
+            lblTotalCost.ForeColor = Color.Blue;
             lblTotalCost.Text = "총 금액: ";
         }
     }
