2aef8e2f441ad5269058396e088c10f384d4aa49
지혁 박 <jhboy0714@gmail.com>
2026-04-09 18:11:53 +0900

    과제4 코딩과 테스트 완료 RadioButton과 CheckBox의 항목을 누르면 listBox에 실시간으로 항목과 값이 표시되도록 함

--- Form1.cs
+++ Form1.cs
@@ -5,54 +5,91 @@ namespace BurgerKiosk
         public Form1()
         {
             InitializeComponent();
+            // 실시간으로 선택 항목을 리스트에 반영하도록 이벤트 연결
+            rdoHamBurger.CheckedChanged += SelectionChanged;
+            rdoBulgogiBurger.CheckedChanged += SelectionChanged;
+            rdoChickenBurger.CheckedChanged += SelectionChanged;
+
+            chkPotato.CheckedChanged += SelectionChanged;
+            chkCola.CheckedChanged += SelectionChanged;
+            chkCheese.CheckedChanged += SelectionChanged;
+            chkSauce.CheckedChanged += SelectionChanged;
+            UpdateOrderList();
         }
 
         public int totalCost { get; private set; }
 
         private void btnOrder_Click(object sender, EventArgs e)
         {
-            if (rdoHamBurger.Checked)
-            {
-                totalCost += 5000;
-                lstOrder.Items.Add("햄버거 5,000원");
-            }
-            else if (rdoBulgogiBurger.Checked)
-            {
-                totalCost += 4000;
-                lstOrder.Items.Add("불고기버거 4,000원");
-            }
-            else if (rdoChickenBurger.Checked)
-            {
-                totalCost += 3000;
-                lstOrder.Items.Add("치킨버거 3,000원");
-            }
-            if (chkPotato.Checked)
-            {
-                totalCost += 3500;
-                lstOrder.Items.Add("감자튀김 3,500원");
-            }
-            if (chkCola.Checked)
-            {
-                totalCost += 2500;
-                lstOrder.Items.Add("콜라 2,500원");
-            }
-            if (chkCheese.Checked)
-            {
-                totalCost += 1500;
-                lstOrder.Items.Add("치즈추가 1,500원");
-            }
-            if (chkSauce.Checked)
-            {
-                totalCost += 500;
-                lstOrder.Items.Add("소스추가 500원");
-            }
-            lblTotalCost.ForeColor = Color.Blue;
-            lblTotalCost.Text = "총 금액: " + totalCost.ToString("N0") + "원";
-
+            // 현재 선택된 항목을 기반으로 실시간 합계가 이미 계산되어 있으므로,
+            // 메뉴가 선택되지 않았다면 경고를 표시하고, 그렇지 않으면 주문 완료 메시지를 보여준다.
             if (!rdoHamBurger.Checked && !rdoBulgogiBurger.Checked && !rdoChickenBurger.Checked)
             {
                 lblTotalCost.Text = "메뉴를 선택하세요.";
                 lblTotalCost.ForeColor = Color.Red;
+                return;
+            }
+
+           
+        }
+
+        private void SelectionChanged(object? sender, EventArgs e)
+        {
+            UpdateOrderList();
+        }
+
+        private void UpdateOrderList()
+        {
+            lstOrder.Items.Clear();
+            int currentTotal = 0;
+
+            if (rdoHamBurger.Checked)
+            {
+                lstOrder.Items.Add("햄버거 5,000원");
+                currentTotal += 5000;
+            }
+            else if (rdoBulgogiBurger.Checked)
+            {
+                lstOrder.Items.Add("불고기버거 4,000원");
+                currentTotal += 4000;
+            }
+            else if (rdoChickenBurger.Checked)
+            {
+                lstOrder.Items.Add("치킨버거 3,000원");
+                currentTotal += 3000;
+            }
+
+            if (chkPotato.Checked)
+            {
+                lstOrder.Items.Add("감자튀김 3,500원");
+                currentTotal += 3500;
+            }
+            if (chkCola.Checked)
+            {
+                lstOrder.Items.Add("콜라 2,500원");
+                currentTotal += 2500;
+            }
+            if (chkCheese.Checked)
+            {
+                lstOrder.Items.Add("치즈추가 1,500원");
+                currentTotal += 1500;
+            }
+            if (chkSauce.Checked)
+            {
+                lstOrder.Items.Add("소스추가 500원");
+                currentTotal += 500;
+            }
+
+            totalCost = currentTotal;
+            if (!rdoHamBurger.Checked && !rdoBulgogiBurger.Checked && !rdoChickenBurger.Checked)
+            {
+                lblTotalCost.Text = "메뉴를 선택하세요.";
+                lblTotalCost.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblTotalCost.ForeColor = Color.Blue;
+                lblTotalCost.Text = "총 금액: " + totalCost.ToString("N0") + "원";
             }
         }
 
@@ -70,7 +107,7 @@ namespace BurgerKiosk
             lstOrder.Items.Clear(); 
             totalCost = 0;
             lblTotalCost.ForeColor = Color.Blue;
-            lblTotalCost.Text = "총 금액: ";
+            lblTotalCost.Text = "총 금액: 0원";
         }
     }
 }
